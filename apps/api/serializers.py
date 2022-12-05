9c7475cc1c28309516804e026bd9b38ee9442a44
felipelara <ext.felipe.l@correos.cl>
2022-12-05 09:35:20 -0300

    correcciones create and update

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,35 +1,38 @@
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 from apps.news.models import New, Image
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
     class Meta:
         model = Image
         fields = ['image']
 
 
 class NewsSerializer(serializers.ModelSerializer):
-    images = ImageSerializer(many=True, read_only=True)
+    main_image = Base64ImageField()
+    images = ImageSerializer(many=True, required=False)
 
     class Meta:
         model = New
         fields = ['id', 'title', 'body', 'tags', 'main_image', 'created_at', 'updated_at', 'images']
 
     def create(self, validated_data):
-        images_data = self.context['request'].FILES
-        new = New.objects.create(**validated_data)
-        for image_data in images_data.getlist('images'):
-            Image.objects.create(new=new, image=image_data)
-        return new
+        images_data = validated_data.pop('images')
+        instance = super().create(validated_data)
+        for image_data in images_data:
+            Image.objects.create(new=instance, **image_data)
+        return instance
 
     def update(self, instance, validated_data):
-        images_data = self.context['request'].FILES
-        instance.title = validated_data.get('title', instance.title)
-        instance.body = validated_data.get('body', instance.body)
-        instance.tags = validated_data.get('tags', instance.tags)
-        instance.main_image = validated_data.get('main_image', instance.main_image)
-        instance.save()
-        for image_data in images_data.getlist('images'):
-            Image.objects.create(new=instance, image=image_data)
+        images_data = validated_data.pop('images')
+        instance = super().update(instance, validated_data)
+        for image_data in images_data:
+            # Acá tendrás que decidir si quieres actualizar o crear
+            #o bien, eliminar todas las imágenes y crearlas de nuevo
+            #image = Image.objects.filter(new=instance)
+            #image.delete()
+            Image.objects.update_or_create(new=instance, **image_data)
         return instance
